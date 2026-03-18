83af56fb6ff28aa22c9657683bd520bcb5d16732
Homely Assist <homeconnecteasy@gmail.com>
2026-03-18 17:41:37 +0530

    commit function and module

--- learning/functions/part2.py
+++ learning/functions/part2.py
@@ -7,12 +7,13 @@
 # part1.f1("Mohit")
 # f1("Mohit")
 
-# import math
+# from math import pi
 #
 # print(math.sqrt(25))
 # print(math.fabs(-34))
+# print(pi)
 
-from datetime import datetime
+from datetime import datetime # (datetime ==> variable, class, function)
 
 print(datetime.now().strftime("%B %d, %Y"))
 print(int(datetime.now().timestamp()))
