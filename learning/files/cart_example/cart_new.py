f5257a92d47d5a07d33763a7ee86683358bc5fb1
mohitsingh <iammohitsingh109@gmail.com>
2026-04-19 19:01:44 +0530

    oops class & object, enscuplation

--- learning/files/cart_example/cart_new.py
+++ learning/files/cart_example/cart_new.py
@@ -5,9 +5,11 @@ name
 email
 Example: 123,Mohit,mohit123@gmail.com
 """
+from learning.oops_learning.person import User
+
 # cache
 """
-[{"user_id": 123,"name": "Mohit","email": "mohit123@gmail.com"}]
+[User(u_id:112, name:Mohit, email:abc)])]
 """
 
 # File read and return all the user
@@ -15,45 +17,40 @@ def load_user() -> list:
     user_list = []
     with open("users.txt", "r") as file:
         for line in file:
-            user_id, name, email = line.split(",") # [123, Mohit, mohit123@gmail.com]
-            user = {
-                "user_id": user_id,
-                "name": name,
-                "email": email
-            }
-            # user = User(user_id, name, email)
+            user_id, name, email = line.split(",")
+            user = User(user_id, name, email)
             user_list.append(user)
     return user_list
 
-def find_user_by_id(user_id: str) -> dict:
+def find_user_by_id(user_id: str) -> User | None:
     if not users:
         print("[WARN] No users database found exit the code")
         exit(-1)
     for user in users:
-        if user["user_id"] == user_id:
-            return user # {"user_id": 123,"name": "Mohit","email": "mohit123@gmail.com"}
+        if user.u_id == user_id:
+            return user
     print(f"[WARN] No user found with id {user_id}")
-    return {}
+    return None
 
-def find_user_by_name(name: str) -> dict:
+def find_user_by_name(name: str) -> User | None:
     if not users:
         print("[WARN] No users database found exit the code")
         exit(-1)
     for user in users:
-        if user["name"] == name:
-            return user  # {"user_id": 123,"name": "Mohit","email": "mohit123@gmail.com"}
+        if user.name == name:
+            return user
     print(f"[WARN] No user found with name {name}")
-    return {}
+    return None
 
-def find_user_by_email(email: str) -> dict:
+def find_user_by_email(email: str) -> User | None:
     if not users:
         print("[WARN] No users database found exit the code")
         exit(-1)
     for user in users:
-        if user["email"] == email:
-            return user  # {"user_id": 123,"name": "Mohit","email": "mohit123@gmail.com"}
+        if user.email == email:
+            return user
     print(f"[WARN] No user found with email {email}")
-    return {}
+    return None
 
 def save_user(user_id, name, email):
     if not users:
@@ -63,25 +60,18 @@ def save_user(user_id, name, email):
     if find_user_by_id(user_id):
         print(f"[WARN] User found with id {user_id} we can't save same user again")
         return
-
-    user_row = f"{user_id},{name},{email}" # 123,Mohit,mohit123@gmail.com
+    user = User(user_id, name, email)
     with open("users.txt", "a") as file:
-        file.write(user_row + "\n")
+        file.write(user.txt_row() + "\n")
 
     # updating our users cache
-    user = {
-        "user_id": user_id,
-        "name": name,
-        "email": email
-    }
-
     users.append(user)
 
 def load_cart() -> dict:
     carts_data = {}
 
     for user in users:
-        carts_data[user["user_id"]] = []
+        carts_data[user.u_id] = []
 
     with open("carts.txt", "r") as file:
         for line in file:
@@ -96,7 +86,8 @@ def load_cart() -> dict:
     return carts_data
 
 users = load_user()
-# print(users)
+for user in users:
+    user.display()
 # save_user("2244", "Aman", "aman123@gmail.com")
 # save_user("2245", "Karan", "karan@gmail.com")
 # print(users)
---
+++ learning/oops_learning/person.py
@@ -0,0 +1,34 @@
+# Class & Object
+# class Person
+# Encapsulation
+class User:
+    # self ==> self refer to current/calling object
+    def __init__(self, u_id, name, email): # constructor
+        self.u_id = u_id
+        self.name = name
+        self.email = email
+
+    def display(self): # class function
+        print(f"User id: {self.u_id}, name: {self.name}, email: {self.email}")
+
+    def txt_row(self):
+        return f"{self.u_id},{self.name},{self.email}"
+
+"""
+2231,Mohit Singh,mohit123@gmail.com
+2244,Aman,aman123@gmail.com
+2245,Karan,karan@gmail.com
+"""
+
+# # object
+# user_1 = User(2231, "Mohit Singh", "mohit123@gmail.com", "ad1")
+# user_2 = User(2244, "Aman", "aman123@gmail.com", "ad2")
+# user_3 = User(2245, "Karan", "karan@gmail.com", "ad3")
+#
+# user_list = [user_1, user_2, user_3]
+#
+# for user in user_list:
+#     user.display()
+#
+# abc = 123
+# abc = str(abc)
