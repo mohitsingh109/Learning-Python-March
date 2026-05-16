b090548d09f639a45d68e933d6783284b3a8e49a
mohitsingh <iammohitsingh109@gmail.com>
2026-05-16 14:12:51 +0530

    oops class & object, enscuplation

---
+++ learning/oops_learning/user_signup.py
@@ -0,0 +1,8 @@
+# user: id, name, password
+# validate() ==> that will validate the password
+# 1. password > 5 char
+# 2. password < 10 char
+# 3. password should contain 1 special char (@,!,#,$,-,_)
+# 4. password should contain 1 int (0-9)
+# 5. password should contain 1 eng (a-z) (A-Z)
+# for & str = 'abcdef....zAbcd...z'
